a810d661334490ef205d6bc54e42911045353a27
Alina Fankhänel <alina.fankhaenel@pb.fhdw.de>
2016-10-19 13:27:53 +0200

    Benutzerklasse

--- Festiva/src/Benutzer.java
+++ Festiva/src/Benutzer.java
@@ -1,4 +1,25 @@
+import java.util.Date;
 
+/**
+ * Fachklasse für einen Benutzer (Kunde oder Administrator) des Webshops
+ * 
+ * @author Alina Fankhänel
+ *
+ */
 public class Benutzer {
-String Name;
+	public int benutzerID;
+	public String vorname;
+	public String nachname;
+	public String eMailAdresse;
+	public String passwortHash;
+	public String strasse;
+	public String hausnummer;
+	public int plz;
+	public String ort;
+	public boolean istGesperrt;
+	public String iban;
+	public String bic;
+	public boolean einzugsermächtigungErteilt;
+	public boolean istGelöscht;
+	public int gruppenID;
 }
