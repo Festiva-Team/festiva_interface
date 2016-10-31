6dfd6eedddef1279310e1da145f82afd7b5e023f
Timo Schlüter <timo.schlueter95@googlemail.com>
2016-10-31 13:26:41 +0100

    Shop Suche

---
+++ Festiva/src/servletPackage/ShopSuche.java
@@ -0,0 +1,51 @@
+package servletPackage;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+*
+* @author Timo Schlüter
+*/
+@WebServlet("/ShopSuche")
+public class ShopSuche extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShopSuche() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String name = request.getParameter("name");
+		String ort = request.getParameter("ort");
+		
+
+		request.getSession(false).setAttribute("name", name);
+		request.getSession(false).setAttribute("ort", ort);
+		request.getRequestDispatcher("k_shop2.jsp").include(request, response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
