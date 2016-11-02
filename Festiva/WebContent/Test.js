e1826905f81cd18c0e71cc774cefae039835dec4
Nicola Kl <Nicola Kl>
2016-11-02 08:59:33 +0100

    Pop-Up Test

--- Festiva/WebContent/Test.js
+++ Festiva/WebContent/Test.js
@@ -1,33 +1,9 @@
 /**
  *  fdhkjlfdfdhjlkdshl
  */
-<script>
-	function dateiauswahl(evt) {
-			var dateien = evt.target.files; // FileList object
-			// Auslesen der gespeicherten Dateien durch Schleife
-			for (var i = 0, f; f = dateien[i]; i++) {
-				// nur Bild-Dateien
-				if (!f.type.match('image.*')) {
-					continue;
-				}
-				var reader = new FileReader();
-				reader.onload = (function (theFile) {
-					return function (e) {
-						// erzeuge Thumbnails.
-						var vorschau = document.createElement('img');
-						vorschau.className = 'vorschau';
-						vorschau.src = e.target.result;
-						vorschau.title = theFile.name;
-						document.getElementById('list')
-							.insertBefore(vorschau, null);
-					};
-				})(f);
-				// Bilder als Data URL auslesen.
-				reader.readAsDataURL(f);
-			}
-		}
-		// Auf neue Auswahl reagieren und gegebenenfalls Funktion dateiauswahl neu ausführen.
-	document.getElementById('files')
-		.addEventListener('change', dateiauswahl, false);
+function del(){
+   if(confirm("Soll der Datensatz wirklich gelöscht werden?") == true)
+      document.form.submit();
+}
 
-</script>
+
--- Festiva/WebContent/a_kundenAendern.jsp
+++ Festiva/WebContent/a_kundenAendern.jsp
@@ -78,7 +78,7 @@ else {
 			</div>
 			<form action="/Festiva/Kundenverwaltung?aktion=loeschen&kundenid=<%=benutzer.id%>" method="post">
 				<div id="spalterechts">
-					<button type="submit" id="links">Kunden löschen</button>
+					<input type="submit" value="l&ouml;schen" onclick="return confirm('Wirklich weghauen das Zeugs?')">
 					</div>
 					</form>
 					<div id="spalterechts">
@@ -95,5 +95,26 @@ else {
 		<footer></footer>
 </div>	
 </body>
+<script type="text/javascript">
+var pop = null;
+
+function popdown() {
+  if (pop && !pop.closed) pop.close();
+}
+
+function popup(obj,w,h) {
+  var url = (obj.getAttribute) ? obj.getAttribute('href') : obj.href;
+  if (!url) return true;
+  w = (w) ? w += 20 : 150;  // 150px*150px is the default size
+  h = (h) ? h += 25 : 150;
+  var args = 'width='+w+',height='+h+',resizable';
+  popdown();
+  pop = window.open(url,'',args);
+  return (pop) ? false : true;
+}
+
+window.onunload = popdown;
+window.onfocus = popdown;
+</script>
 </html>
 <% request.getSession().removeAttribute("benutzer");}%>
